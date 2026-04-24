351200707108130e7ba47ec4d43602198f5e984c
Trafiganty <fonyuyjudegita@gmail.com>
2026-04-24 15:56:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,13 +7,15 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
       },
       {
-         protocol: 'https',
-         hostname: 'media.dev.to',
+        protocol: 'https',
+        hostname: 'media.dev.to',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co', // <-- this is what you were missing
       }
     ],
   },
 };
 
 module.exports = nextConfig;
-
-
